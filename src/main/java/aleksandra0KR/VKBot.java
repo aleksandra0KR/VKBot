24d2e4b75bb95f1b266480b865d864b8ec393865
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-06-11 23:22:33 +0300

    feat: added entry point

---
+++ src/main/java/aleksandra0KR/VKBot.java
@@ -0,0 +1,14 @@
+package aleksandra0KR;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VKBot {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VKBot.class, args);
+  }
+
+}
